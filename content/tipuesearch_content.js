afa19ab7b82707acc9eb67b747b0235447653feb
mamark888 <mamaek888@gmail.com>
2023-04-17 16:03:15 +0800

    加上 cmsimde 目錄架構,並且修改網頁標題, 然後以 token 進行 push

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': '倉儲: https://github.com/mdewcm2023/site-mamark888 \n 網站: https://mdewcm2023.github.io/site-mamark888 \n', 'tags': '', 'url': 'About.html'}, {'title': '權限', 'text': '近端倉儲的設定檔案: 倉儲目錄下 .git 中的 config,代表倉儲的 configuration file (設定檔案) \n', 'tags': '', 'url': '權限.html'}, {'title': '提交', 'text': 'commit \n git commit -m \n message \n 指令執行時,隨後要接上提交文字串, 說明本次提交的改版說明', 'tags': '', 'url': '提交.html'}]};
